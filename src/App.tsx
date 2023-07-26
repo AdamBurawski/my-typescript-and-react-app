aa5f3add61fc73c543ebf50a8a3135c3554c3aa1
AdamBurawski <a.burawski.post@gmail.com>
2023-07-26 09:47:26 +0200

    app updated cryptoPrice

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { CryptoPrice } from "./components/cryptoPrice/CryptoPrice";
 
 import { Dialog } from "./components/common/Dialog/Dialog";
 
 export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Dialog title="xyz">
-          <h1>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa
-            consequuntur quo nobis quidem suscipit iste officiis commodi, quia
-            nesciunt fuga minus in nemo vitae consequatur, distinctio beatae
-            optio amet deserunt?
-          </h1>
-        </Dialog>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //     <Dialog title="xyz">
+    //       <h1>
+    //         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa
+    //         consequuntur quo nobis quidem suscipit iste officiis commodi, quia
+    //         nesciunt fuga minus in nemo vitae consequatur, distinctio beatae
+    //         optio amet deserunt?
+    //       </h1>
+    //     </Dialog>
+    //   </header>
+    // </div>
+    <CryptoPrice />
   );
 };
---
+++ src/components/cryptoPrice/CryptoPriceOfOnePair.tsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import { BinanceOneCryptoPairData } from "../../types/crypto-data";
+
+interface Props {
+  onePair: BinanceOneCryptoPairData;
+}
+
+export const CryptoPriceOfOnePair = (props: Props) => (
+  <p
+    style={{
+      backgroundColor:
+        Number(props.onePair.priceChangePercent) > 0 ? "green" : "red",
+    }}
+  >
+    {props.onePair.symbol} aktualnie jest sprzedawane za średnioważoną cenę{" "}
+    {props.onePair.weightedAvgPrice}
+  </p>
+);
